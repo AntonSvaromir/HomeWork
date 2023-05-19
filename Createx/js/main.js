32898115491b476e222ded38f98e4a78a6251f6e
AntonSvaromir <130767595+AntonSvaromir@users.noreply.github.com>
2023-05-19 16:19:25 +0300

    Add files via upload

--- Createx/js/main.js
+++ Createx/js/main.js
@@ -24,4 +24,44 @@ $(function  () {
       e.preventDefault()
       $('.team__slider').slick('slickNext')
    })
+
+
+
+   $('.testimonials__slider').slick({
+      arrows: false,
+      dots: true,
+      appendDots: $('.testimonials__dots'),
+      waitForAnimate: false,
+   })
+
+   $('.testimonials__arrow__prev').on('click', function (e) {
+      e.preventDefault()
+      $('.testimonials__slider').slick('slickPrev')
+   })
+   $('.testimonials__arrow__next').on('click', function (e) {
+      e.preventDefault()
+      $('.testimonials__slider').slick('slickNext')
+   })
+
+
+
+   $('.program__accordeon-link').on('click', function (e) {
+      e.preventDefault()
+      if($(this).hasClass('program__accordeon-link--active')) {
+         $(this).removeClass('program__accordeon-link--active')
+         $(this).children('.program__acc-text').slideUp()
+      } else {
+         $('.program__accordeon-link').removeClass('program__accordeon-link--active')
+         $('.program__acc-text').slideUp()
+         $(this).addClass('program__accordeon-link--active')
+         $(this).children('.program__acc-text').slideDown()
+      }
+
+   })
+
+   // $('.program__accordeon-link').on('click', function (e) {
+   //    e.preventDefault()
+   //    $(this).toggleClass('program__accordeon-link--active')
+   //    $(this).children('.program__acc-text').slideToggle()
+   // })
 })
