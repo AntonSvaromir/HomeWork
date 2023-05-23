c3f26e961b2091b19c974f6aed67b3328292de9c
AntonSvaromir <130767595+AntonSvaromir@users.noreply.github.com>
2023-05-23 23:26:43 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -14,6 +14,31 @@ $(function  () {
       draggable: false,
       appendArrows: $(`.team__slider-arrows`),
       waitForAnimate: false,
+      responsive:
+      [
+         {
+            breakpoint: 1100,
+            settings: {
+               slidesToShow: 3
+            }
+         },
+         {
+            breakpoint: 750,
+            settings: {
+               slidesToShow: 2
+            }
+         },
+         {
+            breakpoint: 550,
+            settings: {
+               slidesToShow: 1,
+               draggable: true,
+               dots: true,
+               appendDots: $('.team__dots'),
+               waitForAnimate: false,
+            }
+         },
+      ]
    })
 
    $('.team__slider-prev').on('click', function (e) {
@@ -32,6 +57,14 @@ $(function  () {
       dots: true,
       appendDots: $('.testimonials__dots'),
       waitForAnimate: false,
+      // [
+      //    // {
+      //    //    breakpoint: 700,
+      //    //    settings: {
+               
+      //    //    }
+      //    // },
+      // ]
    })
 
    $('.testimonials__arrow__prev').on('click', function (e) {
@@ -67,7 +100,7 @@ $(function  () {
 
    $(".header__nav-list a, .header__top-btn, .footer__bottom-linck").on("click", function (e) {
 		//отменяем стандартную обработку нажатия по ссылке
-		e.preventDefault();
+		e.preventDefault()
 
 		//забираем идентификатор бока с атрибута href
 		var id  = $(this).attr('href'),
@@ -76,6 +109,25 @@ $(function  () {
 			top = $(id).offset().top
 		
 		//анимируем переход на расстояние - top за 1500 мс
-		$('body,html').animate({scrollTop: top-60}, 1000);
-	});
+		$('body,html').animate({scrollTop: top-60}, 1000)
+   })
+
+   setInterval(() => {
+      if ($(window).scrollTop() > 0 && $('.header__top').hasClass('header__top--open') === false) {
+         $('.burger').addClass('burger--follow')
+      } else {
+         $('.burger').removeClass('burger--follow')
+      }
+   }, 0)
+   
+   $('.burger, .overlay, .header__top a').on('click', function (e) {
+      e.preventDefault()
+      $('.header__top').toggleClass('header__top--open')
+      $('.overlay').toggleClass('overlay--show')
+      $('.burger').toggleClass('burger--click')
+   })
+
+   $('.footer__top-title--slide').on('click', function () {
+      $(this).next().slideToggle()
+   })
 })
